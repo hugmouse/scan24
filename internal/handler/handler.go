cf710f376bc6150b9fbeb421b8a9924cc284d5bf
hugmouse <me@mysh.dev>
2025-06-04 19:48:07 +0200

    refactor: add license, HTMX and more
    - Separated templates from handler.go into templates.go (still uses same namespace, but a little bit more convinient)
    - Added includeJS custom function for including JS files
    - HTMX is going to be used for progressive enhancement, right now it is underutilized
    - Added LICENSE (MIT + 0BSD from HTMX)

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/hugmouse/scan24/internal/parser"
-	"github.com/hugmouse/scan24/static"
-	"github.com/hugmouse/scan24/templates"
 	"golang.org/x/net/html/charset"
 	"html/template"
 	"io"
@@ -41,32 +39,6 @@ var (
 	tmplIndex  *template.Template
 	tmplResult *template.Template
 )
-
-func init() {
-	funcs := template.FuncMap{
-		"include": func(path string) template.HTML {
-			bb, err := static.FS.ReadFile(path)
-			if err != nil {
-				panic(err)
-			}
-			return template.HTML(bb)
-		},
-	}
-
-	baseTmpl := template.New("").Funcs(funcs)
-
-	var err error
-
-	tmplIndex, err = baseTmpl.New("index.gohtml").ParseFS(templates.FS, "index.gohtml")
-	if err != nil {
-		log.Fatalf("Failed to parse index.gohtml: %v", err)
-	}
-
-	tmplResult, err = baseTmpl.New("result.gohtml").ParseFS(templates.FS, "result.gohtml")
-	if err != nil {
-		log.Fatalf("Failed to parse result.gohtml: %v", err)
-	}
-}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
---
+++ internal/handler/templates.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"github.com/hugmouse/scan24/static"
+	"github.com/hugmouse/scan24/templates"
+	"html/template"
+	"log"
+)
+
+func init() {
+	funcs := template.FuncMap{
+		"include": func(path string) template.HTML {
+			bb, err := static.FS.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
+			return template.HTML(bb)
+		},
+		"includeJS": func(path string) template.JS {
+			bb, err := static.FS.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
+			return template.JS(bb)
+		},
+	}
+
+	baseTmpl := template.New("").Funcs(funcs)
+
+	var err error
+
+	tmplIndex, err = baseTmpl.New("index.gohtml").ParseFS(templates.FS, "index.gohtml")
+	if err != nil {
+		log.Fatalf("Failed to parse index.gohtml: %v", err)
+	}
+
+	tmplResult, err = baseTmpl.New("result.gohtml").ParseFS(templates.FS, "result.gohtml")
+	if err != nil {
+		log.Fatalf("Failed to parse result.gohtml: %v", err)
+	}
+}
